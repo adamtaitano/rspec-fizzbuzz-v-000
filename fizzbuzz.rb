62bad64f1113a86363feafc10b95a6cc8a3c32d1
Adam Taitano <adam.taitano@gmail.com>
2018-06-22 17:28:46 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
-  if (num % 3) && (num % 5) == 0num % 3 == 0
+  if (num % 3) && (num % 5) == 0
     "Fizz"
   elsif num % 5 == 0
     "Buzz"
-  elsif 
+  elsif num % 3 == 0
     "FizzBuzz"
   else
     nil
